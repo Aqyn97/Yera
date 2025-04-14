93cbbfbed89057655278d82cc56dd2d705735383
YerassylTL <151902970+YerassylTL@users.noreply.github.com>
2025-04-15 00:03:24 +0500

    gg

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+
+class Main{
+    public static void main(String[] args) {
+//        String s = "1241";
+//        String b = "23141";
+//        int a = Integer.parseInt(s);
+//        int q = Integer.parseInt(b);
+//        System.out.println(a * q);
+
+//        String sl ="This is John Doe";
+//        String otvet = sl.substring(8,12);
+//        System.out.println(otvet);
+//        System.out.println(sl.split(" ")[2]);
+
+
+
+
+    }
+}
