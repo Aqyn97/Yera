cfb75351a35c559de36639fc57d5e4e20181eefb
YerassylTL <151902970+YerassylTL@users.noreply.github.com>
2025-04-18 00:24:02 +0500

    new one2

--- src/Amamn.java
+++ src/Amamn.java
@@ -2,9 +2,18 @@ import java.sql.SQLOutput;
 
 public class Amamn {
     public static void main(String[] args) {
-        System.out.println("berkut");
-        System.out.println("nnn");
-        System.out.println("JFGDIHGDN");
-        System.out.println("JFGDIHGDN");
+//        System.out.println("berkut");
+////        System.out.println("nnn");
+////        System.out.println("JFGDIHGDN");
+////        System.out.println("JFGDIHGDN");
+        int a = 2;
+        int b = 0;
+        if(a * b % 2 == 0){
+            System.out.printf("yes");
+        }
+        else{
+            System.out.println("no");
+        }
+        System.out.println(a * b % 2);
     }
 }
